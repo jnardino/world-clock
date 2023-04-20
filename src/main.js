892ac625b221a2f453b073ca102a5a2e72c22d1a
Kayleonia <julie_nardino@hotmail.com>
2023-04-19 19:41:52 -0700

    Added select functionality + Fixed setInterval bug

--- src/main.js
+++ src/main.js
@@ -1,19 +1,47 @@
 
 function setTimeDate(location, timezone) {
+    let cityElement = document.querySelector(`${location} .city-date`);
     let cityTimeElement = document.querySelector(`${location} .city-time`);
     let cityTime = moment().tz(timezone);
-    cityTimeElement.innerHTML = `${cityTime.format("h:mm:ss")} <span class="am-pm">${cityTime.format("A")}</span>`;
-    
-    let cityElement = document.querySelector(`${location} .city-date`);
-    cityElement.innerHTML = `${cityTime.format("MMMM Do YYYY")}`;
+
+    if (cityElement !== null || cityTimeElement !== null) {
+        cityTimeElement.innerHTML = `${cityTime.format("h:mm:ss")} <span class="am-pm">${cityTime.format("A")}</span>`;
+        cityElement.innerHTML = `${cityTime.format("MMMM Do YYYY")}`;
+    }
 }
 
-setTimeDate("#new-york", "America/New_York");
-setTimeDate("#london", "Europe/London");
-setTimeDate("#hong-kong", "Asia/Hong_Kong");
-setTimeDate("#tokyo", "Asia/Tokyo");
+function updateCityList(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTimeUpdate = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector(".city-main");
+    citiesElement.innerHTML = `
+    <div class="city-card">
+        <div>
+            <div class="city-name">${cityName}</div>
+            <div class="city-date">${cityTimeUpdate.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="city-time">${cityTimeUpdate.format("h:mm:ss")} 
+            <span class="am-pm">${cityTimeUpdate.format("A")}</span>
+        </div>
+    </div>
+    `;
+}
 
-setInterval(setTimeDate, 1000, "#new-york", "America/New_York");
-setInterval(setTimeDate, 1000, "#london", "Europe/London");
-setInterval(setTimeDate, 1000, "#hong-kong", "Asia/Hong_Kong");
-setInterval(setTimeDate, 1000, "#tokyo", "Asia/Tokyo");
+
+function main() {
+    let citySelectElement = document.querySelector("#city-select");
+    citySelectElement.addEventListener("change", updateCityList);
+    
+    setTimeDate("#new-york", "America/New_York");
+    setTimeDate("#london", "Europe/London");
+    setTimeDate("#hong-kong", "Asia/Hong_Kong");
+    setTimeDate("#tokyo", "Asia/Tokyo");
+
+    setInterval(setTimeDate, 1000, "#new-york", "America/New_York");
+    setInterval(setTimeDate, 1000, "#london", "Europe/London");
+    setInterval(setTimeDate, 1000, "#hong-kong", "Asia/Hong_Kong");
+    setInterval(setTimeDate, 1000, "#tokyo", "Asia/Tokyo");
+}
+
+main();
